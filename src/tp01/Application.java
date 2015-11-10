4a777167ba583757429ff36521808fe2e2637d8b
PandoonQTN <quentin.doucet@outlook.fr>
2015-11-10 17:06:28 +0100

    Fin TP02

--- src/tp01/Application.java
+++ src/tp01/Application.java
@@ -1,7 +1,11 @@
 package tp01;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
+
+import Exception.NombreDeCompteTropEleveException;
+import Exception.SoldeNegatifException;
 
 public class Application {
 
@@ -12,10 +16,96 @@ public class Application {
 	}
 
 	public void demarrer() {
-		menu();
+		System.out
+				.println("\t \t Bienvenue dans la gestion de votre compte Bancaire");
+		System.out
+				.println("Aprennons d'abord a nous connaitre \nVous êtes (Nom + Prénom) ? ");
+		Scanner sc = new Scanner(System.in);
+		String pers = sc.nextLine();
+		String tab[] = pers.split("\\s");
+
+		try {
+
+			System.out.println("Bien " + tab[1] + " " + tab[0]
+					+ ", sans indiscrétion pourrais-je connaitre votre âge ? ");
+
+			int age = -1;
+			while (age < 0) {
+				sc = new Scanner(System.in);
+				if (sc.hasNextInt()) {
+					age = sc.nextInt();
+				} else {
+					System.out.println("Faites un petit effort :) ");
+				}
+			}
+
+			Personne p = new Personne(1, tab[0], tab[1], age, "tot@mail.fr");
+			p.nouveauCompte(1000.00, "A");
+			p.nouveauCompte(100.0, "B");
+			p.nouveauCompte(10.0, "C");
+
+			Personne p1 = new Personne(2, "AiMARRE", "Jean", 12,
+					"AiMARRE@mail.fr");
+			p1.nouveauCompte(1000.00, "A");
+			p1.nouveauCompte(100.0, "B");
+			p1.nouveauCompte(10.0, "C");
+
+			Personne p2 = new Personne(3, "COVERT", "Harry", 14,
+					"COVERT@mail.fr");
+			p2.nouveauCompte(1000.00, "A");
+			p2.nouveauCompte(100.0, "B");
+			p2.nouveauCompte(10.0, "C");
+			
+			this.aouterClient(p);
+			this.aouterClient(p1);
+			this.aouterClient(p2);
+
+			while (true) {
+				int res = menu();
+
+				switch (res) {
+				case 1:
+					for (Personne tpers : this.tabClients) {
+						tpers.visualiserCompte();
+					}
+					break;
+				case 2:
+					int numeroClient = selectClient();
+					int nb = 0;
+					do {
+						System.out.println();
+						System.out.println("\t1. Voir le solde du compte");
+						System.out.println("\t2. Retirer un montant");
+						System.out.println("\t3. Faire un apport");
+						System.out.println("\t5. Ajouter un compte");
+						System.out.println("\t6. Quitter");
+						System.out.println();
+						sc = new Scanner(System.in);
+
+						if (sc.hasNextInt()) {
+							nb = sc.nextInt();
+						} else {
+							System.err.println("Il faut saisir un nombre !");
+						}
+
+						faireOperation(recupererClient(numeroClient), nb);
+					} while (nb != 6);
+					break;
+
+				default:
+					break;
+				}
+
+			}
+		} catch (IndexOutOfBoundsException e) {
+			System.out
+					.println("Il faut saisir le nom ET le prénom pour l'ouverture d'un compte bancaire. \nRechargez l'application");
+		}
+
 	}
 
 	public int menu() {
+		System.out.println("-->");
 		System.out.println("1. Afficher tous les clients et leurs comptes.");
 		System.out.println("2. Faire une opération sur le compte d’un client.");
 		System.out.println("3. Quitter.");
@@ -34,8 +124,127 @@ public class Application {
 		this.tabClients.add(p);
 	}
 
-	public boolean faireOperation(String numClient, String numCompte,
-			int typeOperation, double montant) {
-		
+	public Personne recupererClient(int num) {
+		for (Personne p : tabClients) {
+			if (p.getNumero() == num) {
+				return p;
+			} 
+		}
+		return null;
+	}
+
+	public int selectClient() {
+		for (Personne p : this.tabClients) {
+			System.out.println(p);
+		}
+		System.out.println("Saisir le numéro du client :");
+		Scanner sc = new Scanner(System.in);
+		if (sc.hasNextInt()) {
+			return sc.nextInt();
+		} else {
+			return -1;
+		}
+
+	}
+
+	public void faireOperation(Personne p, int nb) {
+		Iterator<CompteBancaire> it;
+		String num;
+		CompteBancaire compteSelect = null;
+
+		switch (nb) {
+		case 1:
+			p.visualiserCompte();
+			break;
+		case 2:
+			Scanner sc = new Scanner(System.in);
+			it = p.getCompteBancaires().iterator();
+			System.out
+					.println("Veillez saisir le numero de compte à utiliser :");
+
+			num = sc.nextLine();
+			compteSelect = null;
+
+			while (it.hasNext()) {
+				CompteBancaire compte = it.next();
+				if (compte.getNumeroDeCompte().equals(num)) {
+					compteSelect = compte;
+					break;
+				}
+			}
+			if (compteSelect == null) {
+				System.out.println("veillez saisir un compte valide");
+			} else {
+				sc = new Scanner(System.in);
+				System.out.println("Combien voulez-vous retirer ?");
+				if (sc.hasNextDouble()) {
+
+					try {
+						compteSelect.RetirerMontant(sc.nextDouble());
+					} catch (SoldeNegatifException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			break;
+		case 3:
+			sc = new Scanner(System.in);
+			it = p.getCompteBancaires().iterator();
+			System.out
+					.println("Veillez saisir le numero de compte a utiliser :");
+			num = sc.nextLine();
+			compteSelect = null;
+
+			while (it.hasNext()) {
+				CompteBancaire compte = it.next();
+				if (compte.getNumeroDeCompte().equals(num)) {
+					compteSelect = compte;
+					break;
+				}
+			}
+			if (compteSelect == null) {
+				System.out.println("Veuillez saisir un compte valide");
+			} else {
+				sc = new Scanner(System.in);
+				System.out.println("Combien voulez-vous ajouter ?");
+				if (sc.hasNextDouble()) {
+					compteSelect.ajouterMontant(sc.nextDouble());
+				}
+			}
+			break;
+		case 4:
+
+			break;
+		case 5:
+			if (p.getNbCompte() <= 10) {
+				System.out.println("Saisir numero de compte : ");
+				sc = new Scanner(System.in);
+				String numCompte = sc.next();
+				System.out.println("Saisir solde du compte : ");
+				sc = new Scanner(System.in);
+				if (sc.hasNextDouble()) {
+					Double solde = sc.nextDouble();
+					p.nouveauCompte(solde, numCompte);
+					System.out.println("Compte créé");
+				} else {
+					System.out.println("Veuillez saisir un nombre!!!!");
+				}
+			} else {
+				try {
+					throw new NombreDeCompteTropEleveException(
+							"Vous possédez déjà le nombre maximal de compte bancaire.");
+				} catch (NombreDeCompteTropEleveException e) {
+					e.printStackTrace();
+				}
+			}
+			break;
+		case 6:
+			break;
+		default:
+			System.err.println("Vous avez saisie n'importe quoi ! Try again ");
+			break;
+
+		}
 	}
 }
--- src/tp01/Personne.java
+++ src/tp01/Personne.java
@@ -10,29 +10,26 @@ public class Personne {
 	private String nom;
 	private String prenom;
 	private int age;
-	private String email; 
-	private ArrayList<CompteBancaire> compteBancaires = new ArrayList<CompteBancaire>(); 
+	private String email;
+	private ArrayList<CompteBancaire> compteBancaires = new ArrayList<CompteBancaire>();
 
-	public Personne(){
-		
+	public Personne() {
+
 	}
-	
+
 	public Personne(int num, String nom, String prenom, int age, String mail) {
 		this.nbCompte = 1;
 		this.numero = num;
-		this.nom = nom; 
-		this.prenom = prenom; 
+		this.nom = nom;
+		this.prenom = prenom;
 		this.age = age;
 		this.email = mail;
 	}
-	
-	
-	public void addCompteBancaire(CompteBancaire cb){
+
+	public void addCompteBancaire(CompteBancaire cb) {
 		this.compteBancaires.add(cb);
 		this.nbCompte++;
 	}
-	
-	
 
 	public int getNbCompte() {
 		return nbCompte;
@@ -73,7 +70,23 @@ public class Personne {
 	public void setCompteBancaires(ArrayList<CompteBancaire> compteBancaires) {
 		this.compteBancaires = compteBancaires;
 	}
-	
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public void nouveauCompte(String n) {
 		CompteBancaire cb = new CompteBancaire(100.0, n);
 		addCompteBancaire(cb);
@@ -83,22 +96,76 @@ public class Personne {
 		CompteBancaire cb = new CompteBancaire(solde, n);
 		addCompteBancaire(cb);
 	}
-	
-	public void visualiserCompte(){
-		System.out.println("\t Visualisation des comptes de "+this.prenom+" "+this.nom);
-		Iterator<CompteBancaire> listeCompte = this.getCompteBancaires().iterator();
-		while(listeCompte.hasNext()){
+
+	public void visualiserCompte() {
+		System.out.println("\t Visualisation des comptes de " + this.prenom
+				+ " " + this.nom);
+		Iterator<CompteBancaire> listeCompte = this.getCompteBancaires()
+				.iterator();
+		while (listeCompte.hasNext()) {
 			CompteBancaire cb = listeCompte.next();
 			System.out.println(cb);
 		}
 	}
-	
+
 	@Override
-	public String toString(){
-		return this.nom;
-		
+	public String toString() {
+		return this.numero + " - " + this.nom + " " + this.prenom + " - "
+				+ this.age + " ans - " + this.email;
+
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result
+				+ ((compteBancaires == null) ? 0 : compteBancaires.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + nbCompte;
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		result = prime * result + numero;
+		result = prime * result + ((prenom == null) ? 0 : prenom.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Personne other = (Personne) obj;
+		if (age != other.age)
+			return false;
+		if (compteBancaires == null) {
+			if (other.compteBancaires != null)
+				return false;
+		} else if (!compteBancaires.equals(other.compteBancaires))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (nbCompte != other.nbCompte)
+			return false;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		if (numero != other.numero)
+			return false;
+		if (prenom == null) {
+			if (other.prenom != null)
+				return false;
+		} else if (!prenom.equals(other.prenom))
+			return false;
+		return true;
+	}
 
 }
