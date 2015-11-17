da94cf096554fc9885ba7ee1227e2e698ea90fdb
PandoonQTN <quentin.doucet@outlook.fr>
2015-11-17 16:29:55 +0100

    commit

--- src/modele/Compte.java
+++ src/modele/Compte.java
@@ -131,11 +131,8 @@ public class Compte {
 	 * 
 	 * @return the dateCrï¿½ation
 	 */
-<<<<<<< HEAD
-	public Date getDateCréation() {
-=======
+
 	public Date getDateCreation() {
->>>>>>> origin/master
 		return dateCreation;
 	}
 
@@ -145,13 +142,9 @@ public class Compte {
 	 * @param dateCreation
 	 *            the dateCrï¿½ation to set
 	 */
-<<<<<<< HEAD
-	public void setDateCréation(Date dateCréation) {
-		this.dateCreation = dateCréation;
-=======
+
 	public void setDateCreation(Date dateCreation) {
 		this.dateCreation = dateCreation;
->>>>>>> origin/master
 	}
 
 }
